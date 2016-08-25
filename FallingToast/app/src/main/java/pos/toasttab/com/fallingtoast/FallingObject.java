ac55b9476f074ca6f134958dfe49f722a8132ee0
Stanislaw Nagiec <stanislaw@heyKidImStanislawsMacBookPro.local>
2016-08-25 16:29:24 -0400

    saving progress on FallingObject.java

--- FallingToast/app/src/main/java/pos/toasttab/com/fallingtoast/FallingObject.java
+++ FallingToast/app/src/main/java/pos/toasttab/com/fallingtoast/FallingObject.java
@@ -1,19 +1,21 @@
 package pos.toasttab.com.fallingtoast;
 
+import java.util.Random;
+
 /**
  * Created by jraffile on 8/25/16.
  */
 public class FallingObject {
 
-    private int objectXLocation;
+//    public int objectXLocation;
     // figure out our object size so the object is not cut off on the screen
-    private int objectSize;
+    public int objectSize;
     // i dont know why the following line is what it is
-    private int objectYLocation = -objectSize;
-    private int fallSpeed = 1;
+    public int objectYLocation = -objectSize;
+    public int fallSpeed = 1;
     Random rand = new Random();
+    public int fallingObjectXLocation;
 
-    //iniital position math.random
     // creates a random value inside the window and stores it in fallingObjectXLocation
     public int generateRandomXLocation() {
         // change Game.WINDOW_WIDTH to whatever Game is in our code
@@ -23,5 +25,12 @@ public class FallingObject {
     // creates a random non-zero vlaue and stores it in fallSpeed
     public int generateRandomFallSpeed() {
         return fallSpeed = rand.ints(1, 1, 10).findFirst().getAsInt();
+    }
+
+    // collide?
+    public boolean hasCollided(Boolean hasCollided) {
+        // if fork hits toaster lives -1
+        // if bread hits toaster score +1
+        return hasCollided(hasCollided);
     }
 }
