5e829a33a0415044ae6c507a5d375c0f80026f6d
Tom Roark <troark@Thomass-MacBook-Air.local>
2016-08-25 15:17:11 -0400

    Jason did most of this part

--- FallingToast/app/src/main/java/pos/toasttab/com/fallingtoast/HealthBar.java
+++ FallingToast/app/src/main/java/pos/toasttab/com/fallingtoast/HealthBar.java
@@ -4,4 +4,18 @@ package pos.toasttab.com.fallingtoast;
  * Created by jraffile on 8/25/16.
  */
 public class HealthBar {
+    int totalHP;
+    int currentHP;
+
+    totalHP = 5
+    currentHP = 5
+
+    public boolean isAlive() {
+        if (currentHP > 0){
+            return true;
+        }else
+            return false
+    }
+                    //method isalive reutrns a boolean if current hp is > 0 return true if not return false
+
 }
